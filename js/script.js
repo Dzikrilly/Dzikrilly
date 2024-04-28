c4de34bdbea8912e6dd340a3209c6597029a1d45
Dzikrilly <163078093+Dzikrilly@users.noreply.github.com>
2024-04-28 14:57:54 +0700

    Add files via upload
    init

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const buttNext = document.querySelector(".nextContent");
+const content2 = document.querySelector(".content2");
+const paper = document.querySelector(".paper");
+// TYPE WRITE PROPERTY
+const textArea = document.querySelector(".pesan");
+const message = `~ untuk: Lili Ikan Sarden  🐟
+
+Selamat ulang tahun oiii🎉🎉🎉......
+Semoga sehat dan bahagia selalu,
+semoga nanti utbk lancar dan mendapatkan skor 700++. (klok udah selesai utbk, minimal kasih contekan lah kwkwk)
+Semoga keterima di Unesa, dan masa-masa kuliahnya penuh bahagia ammin...
+
+Dari : peternak kucing 🐈
+`;
+let i = 0;
+// ================
+
+buttNext.addEventListener("click", () => {
+    content2.style.display = "block";
+    setTimeout(typeWrite, 1500);
+});
+document.addEventListener("click", (e) => {
+    if (e.target == content2) {
+        content2.style.display = "none";
+    }
+});
+
+function typeWrite() {
+    if (i < message.length) {
+        textArea.innerHTML += message.charAt(i);
+        i++;
+        setTimeout(typeWrite, 55);
+    }
+}
